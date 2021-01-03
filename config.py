09ff0b1bd7a1dd8a683728702f4b9737c97f70b4
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-03 19:00:35 +0300

    Removed f-string

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 database_name = "data.db" # To work on localhost !!
 data_txt_name = "data.txt"
 
-url_api_backend = f"http://api.depremneredeoldu.com/earthquakes"
+url_api_backend = "https://api.depremneredeoldu.com/earthquakes"
