313bb54ae89706b365c5a471237331f8200fd9ff
Alican Yüksel <alicanyuksel@outlook.com>
2021-09-24 10:02:44 +0200

    Update scraping script

--- initialize_db.py
+++ initialize_db.py
@@ -1,19 +1,35 @@
-from app import app
-from db import db
 from config import data_txt_name
 from extract_data import extract_from_web, clean_data_extracted, create_dict
-from models.earthquake import EarthquakeModel
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+URL_GET_BACKEND_PROD = os.getenv("URL_GET_API_BACKEND_PROD")
+URL_POST_BACKEND_PROD = os.getenv("URL_POST_API_BACKEND_PROD")
+
+def get_all_earthquakes(url):
+    r = requests.get(url)
+
+    response = r.json()
+
+    all_earthquakes = response["earthquakes"]
+
+    all_earthquakes_id = []
+    for earthquake in all_earthquakes:
+        all_earthquakes_id.append(earthquake["earthquake_id"])
+
+    return all_earthquakes_id
 
 
-def create_tables(data_json):
-    with app.app_context():
-        db.init_app(app)
-        db.drop_all()
-        db.create_all()
+def save_to_db(data, url):
+    all_earthquakes_list = get_all_earthquakes(url=URL_GET_BACKEND_PROD)
 
-        for data in data_json:
-            earthquake = EarthquakeModel(**data)
-            earthquake.save_to_db()
+    for earthquake in data:
+        earthquake_id = earthquake["earthquake_id"]
+        # check if a new earthquake exists
+        if earthquake_id not in all_earthquakes_list:
+            r_post = requests.post(url, json=earthquake)
 
 
 if __name__ == "__main__":
@@ -27,8 +43,4 @@ if __name__ == "__main__":
     all_data_json = create_dict(file_name=data_txt_name)
 
     # create db from json
-    create_tables(all_data_json)
-
-
-
-
+    save_to_db(data=all_data_json, url=URL_POST_BACKEND_PROD)
