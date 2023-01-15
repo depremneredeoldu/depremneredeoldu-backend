c24149fae29f53c92823245d9c353da664608530
alicanyuksel <alicanyuksel@outlook.com>
2023-01-15 04:11:11 +0100

    Add API prefix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.api_v1 import api_router
+from app.api.api import api_router
 from app.core.config import settings
 
 app = FastAPI(
@@ -16,4 +16,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_router)
+app.include_router(api_router, prefix=settings.API_V1_PREFIX)
