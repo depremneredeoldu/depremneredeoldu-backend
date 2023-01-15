dd5b8dacffe4ed433a0064b30c134fbfaed61f70
alicanyuksel <alicanyuksel@outlook.com>
2023-01-15 04:11:27 +0100

    Remove temporarly API key

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,8 @@
 from app.api.endpoints import earthquake
-from app.core.security import check_api_key
-from fastapi import APIRouter, Security
 
-api_router = APIRouter(dependencies=[Security(check_api_key)])
+# from app.core.security import check_api_key
+from fastapi import APIRouter  # Security
+
+# api_router = APIRouter(dependencies=[Security(check_api_key)])
+api_router = APIRouter()
 api_router.include_router(earthquake.router, tags=["earthquake"])
