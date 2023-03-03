1e5a4e36fce38578c8cb330ef79e096b2e219515
alicanyuksel <alicanyuksel@outlook.com>
2023-03-03 01:40:51 +0100

    Update debug db path

--- app/core/database.py
+++ app/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN", "sqlite:///database.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN", "sqlite:///db/database.db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
