54aef32f4116523a4721489ba96165869e243394
alicanyuksel <alicanyuksel@outlook.com>
2024-01-20 23:45:28 +0100

    Update api endpoints with firestore

--- app/api/endpoints/earthquake.py
+++ app/api/endpoints/earthquake.py
@@ -1,28 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
 from fastapi.responses import JSONResponse
-from app.schemas.earthquake import EarthquakeModel
-from sqlalchemy.orm import Session
-from app.core.database import SessionLocal
-from app.core.database import Base, engine
-from app.crud import earthquake as earthquake_crud
-from fastapi import Security
-from app.core.security import check_api_key_post_endpoints
+from google.cloud import firestore_v1
 
-Base.metadata.create_all(bind=engine)
+from app.core.database import get_firestore_db_object
+from app.core.security import check_api_key_post_endpoints
+from app.crud import earthquake as earthquake_crud
+from app.schemas.earthquake import EarthquakeModel
+
 router = APIRouter()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @router.get("/earthquakes")
-async def get_earthquakes(db: Session = Depends(get_db), limit: int = None):
+async def get_earthquakes(
+    db: firestore_v1.client.Client = Depends(get_firestore_db_object), limit: int = None
+):
     all_earthquakes_obj = earthquake_crud.get_earthquakes(db=db, limit=limit)
     return {
         "earthquakes": [each_earthquake for each_earthquake in all_earthquakes_obj],
@@ -33,12 +24,14 @@ async def get_earthquakes(db: Session = Depends(get_db), limit: int = None):
 @router.post("/earthquake")
 async def create_earthquake(
     earthquake: EarthquakeModel,
-    db: Session = Depends(get_db),
+    db: firestore_v1.client.Client = Depends(get_firestore_db_object),
     api_key: str = Security(check_api_key_post_endpoints),
 ) -> JSONResponse:
-    earthquake_obj = earthquake_crud.get_earthquake(db=db, earthquake_id=earthquake.earthquake_id)
-    if earthquake_obj:
+    earthquake_obj = earthquake_crud.get_earthquake(
+        db=db, earthquake_id=earthquake.earthquake_id
+    )
+    if earthquake_obj is not None:
         return JSONResponse({"message": "Earthquake already exists."}, 409)
 
-    earthquake_crud.create_earthquake(db=db, earthquake=earthquake)
+    earthquake_crud.insert_earthquake(db=db, earthquake=earthquake)
     return JSONResponse({"message": "Earthquake added successfully."}, 201)
