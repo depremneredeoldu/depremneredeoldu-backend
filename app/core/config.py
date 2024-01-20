87dc66cb5653e7f041f7d855943caefd30056916
alicanyuksel <alicanyuksel@outlook.com>
2024-01-20 23:51:37 +0100

    Update config

--- app/core/config.py
+++ app/core/config.py
@@ -13,9 +13,10 @@ class Settings(BaseSettings):
     API_KEY_HEADER_NAME: str = "x-api-key"
     API_KEY_GET: str = os.getenv("API_KEY_GET")
     API_KEY_POST: str = os.getenv("API_KEY_POST")
-    COLLECTION_NAME = os.getenv("COLLECTION_NAME")
-    DOCUMENT_NAME = os.getenv("DOCUMENT_NAME")
-    NB_EARTHQUAKES_TO_STOCK_IN_FIRESTORE = 2000
+    FIRESTORE_CREDENTIALS_PATH_FOR_DEV: str = os.getenv("FIRESTORE_CREDENTIALS_FOR_DEV")
+    COLLECTION_NAME: str = os.getenv("COLLECTION_NAME")
+    DOCUMENT_NAME: str = os.getenv("DOCUMENT_NAME")
+    NB_EARTHQUAKES_TO_STOCK_IN_FIRESTORE: int = 2000
 
 
 settings = Settings()
